df896aa8f27e03cf36dc7b6b87d060bd94839d48
Alexander <1348877@senati.pe>
2025-04-10 16:40:36 -0500

    Cambios en los imports

---
+++ Snake-main/main.py
@@ -0,0 +1,3 @@
+import turtle                           #La libreria turtle ayuda a construir la interfaz gráfica
+import time                             #Para obtenier el tiempo
+import random                           #Para usar numeros random
