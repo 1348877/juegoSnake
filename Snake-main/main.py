2f912c489294fe10878d676e3345b7349b57fe8b
Alexander <1348877@senati.pe>
2025-04-10 16:42:53 -0500

    Cambios realizados en main, de rama alterna

--- Snake-main/main.py
+++ Snake-main/main.py
@@ -1,3 +1,14 @@
 import turtle                           #La libreria turtle ayuda a construir la interfaz gráfica
 import time                             #Para obtenier el tiempo
 import random                           #Para usar numeros random
+
+posponer = 0.1
+puntaje = 0
+maxPuntaje = 0
+
+#Configuración
+window = turtle.Screen()                #Crea una ventana nueva
+window.title('Snake')                   #Ponemos titulo
+window.bgcolor('#353535')               #Color de fondo
+window.setup(width=600,height=600)      #Redimensionar pantalla
+window.tracer(0)                        #Ayuda a hacer la animación mas placentera
